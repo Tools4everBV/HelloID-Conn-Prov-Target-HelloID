fb0a43921f55e8369c1abdf26b1677ce91edbc26
msheldont4e <m.sheldon@tools4ever.com>
2020-08-18 07:48:14 -0700

    Create revokePermission.ps1

---
+++ revokePermission.ps1
@@ -0,0 +1,61 @@
+$portalBaseUrl = "https://<customer>.helloid.com"
+$HelloIDApiKey = "<Provide your API key here>"
+$HelloIDApiSecret = "<Provide your API secret here>"
+ 
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+ 
+#Initialize default properties
+$success = $False;
+$auditMessage = "Membership for person " + $p.DisplayName + " not removed successfully";
+ 
+$p = $person | ConvertFrom-Json;
+$m = $manager | ConvertFrom-Json;
+$aRef = $accountReference | ConvertFrom-Json;
+$mRef = $managerAccountReference | ConvertFrom-Json;
+$pRef = $permissionReference | ConvertFrom-json;
+ 
+if(-Not($dryRun -eq $True)) {
+    try
+    {
+        # Create authorization headers with HelloID API key
+        $pair = "${HelloIDApiKey}:${HelloIDApiSecret}"
+        $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+        $base64 = [System.Convert]::ToBase64String($bytes)
+        $key = "Basic $base64"
+ 
+        $headers = @{"authorization" = $Key}
+ 
+        if($PortalBaseUrl.EndsWith("/") -eq $false)
+        {
+            $PortalBaseUrl = $PortalBaseUrl + "/"
+        }
+        $uri = ($PortalBaseUrl +"api/v1/groups/$($pRef.Id)/users/$($aRef)")
+ 
+        $response = Invoke-RestMethod -Uri $uri -Method DELETE -Headers $headers
+        $success = $True;
+        $auditMessage = " successfully";
+    }catch
+    {
+            if(-Not($_.Exception.Response -eq $null)){
+                $result = $_.Exception.Response.GetResponseStream()
+                $reader = New-Object System.IO.StreamReader($result)
+                $reader.BaseStream.Position = 0
+                $reader.DiscardBufferedData()
+                $errResponse = $reader.ReadToEnd();
+                $auditMessage = " : ${errResponse}";
+            } else {
+                $auditMessage = " : General error";
+            }
+    }
+}
+ 
+ 
+#build up result
+$result = [PSCustomObject]@{
+    Success= $success;
+    AuditDetails=$auditMessage;
+    Account = $account;
+};
+ 
+Write-Output $result | ConvertTo-Json -Depth 10;  
