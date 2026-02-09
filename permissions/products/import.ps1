fe39e4961e2fb5e098554367b7b9f1fc2492e99d
mverbraak-t4e <m.verbraak@tools4ever.com>
2026-02-09 16:37:06 +0100

    Add import permission script for products

---
+++ permissions/products/import.ps1
@@ -0,0 +1,235 @@
+####################################################################
+# HelloID-Conn-Prov-Target-HelloID-ImportPermissions-Products
+# PowerShell V2
+####################################################################
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($actionContext.Configuration.isDebug) {
+    $true { $VerbosePreference = "Continue" }
+    $false { $VerbosePreference = "SilentlyContinue" }
+}
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
+#region functions
+function Invoke-HelloIDRestMethod {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Method,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Uri,
+
+        [object]
+        $Body,
+
+        [string]
+        $ContentType = "application/json",
+
+        [Parameter(Mandatory)]
+        [System.Collections.IDictionary]
+        $Headers,
+
+        [Parameter()]
+        [Boolean]
+        $UsePaging = $false,
+
+        [Parameter()]
+        [Int]
+        $Skip = 0,
+
+        [Parameter()]
+        [Int]
+        $Take = 1000,
+
+        [Parameter()]
+        [Int]
+        $TimeoutSec = 60
+    )
+
+    process {
+        try {
+            $splatParams = @{
+                Uri             = $Uri
+                Headers         = $Headers
+                Method          = $Method
+                ContentType     = $ContentType
+                TimeoutSec      = 60
+                UseBasicParsing = $true
+                Verbose         = $false
+                ErrorAction     = "Stop"
+            }
+
+            if ($Body) {
+                Write-Verbose "Adding body to request in utf8 byte encoding"
+                $splatParams["Body"] = ([System.Text.Encoding]::UTF8.GetBytes($Body))
+            }
+
+            if ($UsePaging -eq $true) {
+                $result = [System.Collections.ArrayList]@()
+                $startUri = $splatParams.Uri
+                do {
+                    $splatParams["Uri"] = $startUri + "?take=$($take)&skip=$($skip)"
+                    $response = (Invoke-RestMethod @splatParams)
+                    if ([bool]($response.PSobject.Properties.name -eq "data")) {
+                        $response = $response.data
+                    }
+                    if ($response -is [array]) {
+                        [void]$result.AddRange($response)
+                    }
+                    else {
+                        [void]$result.Add($response)
+                    }
+        
+                    $skip += $take
+                } while (($response | Measure-Object).Count -eq $take)
+            }
+            else {
+                $result = Invoke-RestMethod @splatParams
+            }
+
+            Write-Output $result
+        }
+        catch {
+            throw $_
+        }
+    }
+}
+function Resolve-HelloIDError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+        if (-not [string]::IsNullOrEmpty($ErrorObject.ErrorDetails.Message)) {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+        }
+        elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            if ($null -ne $ErrorObject.Exception.Response) {
+                $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+                if (-not [string]::IsNullOrEmpty($streamReaderResponse)) {
+                    $httpErrorObj.ErrorDetails = $streamReaderResponse
+                }
+            }
+        }
+        try {
+            $errorDetailsObject = ($httpErrorObj.ErrorDetails | ConvertFrom-Json)
+            # error message can be either in [resultMsg] or [message]
+            if ([bool]($errorDetailsObject.PSobject.Properties.name -eq "resultMsg")) {
+                $httpErrorObj.FriendlyMessage = $errorDetailsObject.resultMsg
+            }
+            elseif ([bool]($errorDetailsObject.PSobject.Properties.name -eq "message")) {
+                $httpErrorObj.FriendlyMessage = $errorDetailsObject.message
+            }
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails # Temporarily assignment
+        }
+        catch {
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails
+            Write-Warning $_.Exception.Message
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
+
+# Create authorization headers with HelloID API key
+try {
+    Write-Verbose "Creating authorization headers with HelloID API key"
+
+    $pair = "$($actionContext.Configuration.apiKey):$($actionContext.Configuration.apiSecret)"
+    $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+    $base64 = [System.Convert]::ToBase64String($bytes)
+    $key = "Basic $base64"
+    $headers = @{"authorization" = $Key }
+
+    Write-Verbose "Created authorization headers with HelloID API key"
+}
+catch {
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq "Microsoft.PowerShell.Commands.HttpResponseException") -or
+        $($ex.Exception.GetType().FullName -eq "System.Net.WebException")) {
+        $errorObj = Resolve-HelloIDError -ErrorObject $ex
+        $auditMessage = "Error creating authorization headers with HelloID API key. Error: $($errorObj.FriendlyMessage)"
+        Write-Warning "Error at Line [$($errorObj.ScriptLineNumber)]: $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    }
+    else {
+        $auditMessage = "Error creating authorization headers with HelloID API key. Error: $($ex.Exception.Message)"
+        Write-Warning "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            # Action  = "" # Optional
+            Message = $auditMessage
+            IsError = $true
+        })
+
+    # Throw terminal error
+    throw $auditMessage 
+}
+
+try {
+    Write-Information 'Starting HelloID permission entitlement import'
+
+    $splatImportAssignmentsParams = @{
+        Uri       = "$($actionContext.Configuration.BaseUrl)/product-assignment"
+        Method    = 'GET'
+        Headers   = $headers
+        UsePaging = $true
+    }
+    $importedAssignments = Invoke-HelloIDRestMethod @splatImportAssignmentsParams
+    $assignments = $importedAssignments | Group-Object -Property productGuid -AsHashTable -AsString
+
+    # Group product assignments by productId while joining userGuid to get a list of assigned userGuids per product
+    $products = $importedAssignments | Select-Object -Property productGuid, productName -Unique
+
+    foreach ($importedProduct in $products) {
+        $userGuids = $assignments[$importedProduct.productGuid] | ForEach-Object { $_.userGuid }
+        $userGuids = @($userGuids | Select-Object -Unique)
+
+        $permission = @{
+            PermissionReference = @{
+                Reference = $importedProduct.productGuid
+            }
+            DisplayName         = "$($importedProduct.productName)"
+            AccountReferences   = $null
+        }
+
+        # The code below splits a list of permission members into batches of 100
+        # Each batch is assigned to $permission.AccountReferences and the permission object will be returned to HelloID for each batch
+        # Ensure batching is based on the number of account references to prevent exceeding the maximum limit of 500 account references per batch
+        $batchSize = 500
+        for ($i = 0; $i -lt $userGuids.Count; $i += $batchSize) {
+            $permission.AccountReferences = $userGuids[$i..([Math]::Min($i + $batchSize - 1, $userGuids.Count - 1))]
+            Write-Output $permission
+        }
+    }
+    Write-Information 'HelloID permission entitlement import completed'
+}
+catch {
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-HelloIDError -ErrorObject $ex
+        Write-Warning "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+        Write-Error "Could not import HelloID permission entitlements. Error: $($errorObj.FriendlyMessage)"
+    }
+    else {
+        Write-Warning "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+        Write-Error "Could not import HelloID permission entitlements. Error: $($ex.Exception.Message)"
+    }
+}
